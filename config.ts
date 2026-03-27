77da3454e04ce9d1a2298d62909f05a17ca5bd2a
Daniel Ene <ionut-daniel644@gmail.com>
2026-03-27 12:14:06 +0200

    change main to master

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@
 
 // GitHub Repository URL
 export const REMOTE_CONFIG_URL = 
-  'https://raw.githubusercontent.com/IonutDaniel99/web-debloater-extension/main/config/scripts-config.json';
+  'https://raw.githubusercontent.com/IonutDaniel99/web-debloater-extension/master/config/scripts-config.json';
 
 // Auto-update interval (hours)
 export const AUTO_UPDATE_INTERVAL_HOURS = 6;
